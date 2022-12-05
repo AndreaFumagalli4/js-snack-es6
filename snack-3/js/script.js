d8df55b987a300a7824b176c96b371e30f6dca31
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-12-05 12:46:55 +0100

    fix snack-3

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -68,7 +68,6 @@ let media;
 
 for ( let i = 0; i < zucchine.length; i++) {
     weight += zucchine[i].peso;
-    media = weight / zucchine.length;
 }
 
-console.log(weight, media);
+console.log(weight, (weight / zucchine.length));
